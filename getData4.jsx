a1751c0bfd43ae349c0374bfa4e20a8bee746926
John Williams <jrwtango@gmail.com>
2020-08-24 15:24:53 -0400

    paging1

--- getData4.jsx
+++ getData4.jsx
@@ -1,3 +1,27 @@
+function paginate(items, pageNumber, pageSize) {
+  const start = (pageNumber - 1) * pageSize;
+  let page = items.slice(start, start + pageSize);
+  return page;
+}
+const Pagination = ({ items, pageSize, onPageChange }) => {
+  const { Button } = ReactBootstrap;
+  if (items.length <= 1) return null;
+  let num = Math.ceil(items.length / pageSize);
+  let pages = Array(num).fill(0);
+  pages = [1, 2, 3];
+  const list = pages.map((page) => {
+    return (
+      <Button key={page} onClick={onPageChange} className="page-item">
+        {page}
+      </Button>
+    );
+  });
+  return (
+    <nav>
+      <ul className="pagination">{list}</ul>
+    </nav>
+  );
+};
 const useDataApi = (initialUrl, initialData) => {
   const { useState, useEffect, useReducer } = React;
   const [url, setUrl] = useState(initialUrl);
@@ -59,18 +83,27 @@ const dataFetchReducer = (state, action) => {
 function App() {
   const { Fragment, useState, useEffect, useReducer } = React;
   const [query, setQuery] = useState("MIT");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
     "https://hn.algolia.com/api/v1/search?query=redux",
     {
       hits: [],
     }
   );
-
+  const handlePageChange = (e) => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+  let page = data.hits;
+  if (page.length >= 1) {
+    page = paginate(page, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
   return (
     <Fragment>
       <form
         onSubmit={(event) => {
-          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+          doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
 
           event.preventDefault();
         }}
@@ -89,12 +122,19 @@ function App() {
         <div>Loading ...</div>
       ) : (
         <ul>
-          {data.hits.map((item) => (
+          {page.map((item) => (
             <li key={item.objectID}>
               <a href={item.url}>{item.title}</a>
             </li>
           ))}
         </ul>
+      )}
+      {data.hits && (
+        <Pagination
+          items={data.hits}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+        ></Pagination>
       )}
     </Fragment>
   );
